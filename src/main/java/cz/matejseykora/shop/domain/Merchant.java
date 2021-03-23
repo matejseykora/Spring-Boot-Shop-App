da4b158cf1d811ca401d36c823c06b6575dfc648
matejseykora <matej.seykora@gmail.com>
2021-03-23 08:41:22 +0100

    added Merchant class

---
+++ src/main/java/cz/matejseykora/shop/domain/Merchant.java
@@ -0,0 +1,21 @@
+package cz.matejseykora.shop.domain;
+
+import lombok.*;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Merchant {
+    @Nullable
+    private Integer id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String email;
+    @NonNull
+    private String adress;
+}
