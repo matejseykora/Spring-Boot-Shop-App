0a78fe6d790425e173907812fd3bbb3236dc2dcd
matejseykora <matej.seykora@gmail.com>
2021-03-23 09:46:58 +0100

    re-added default test with example

--- src/test/java/cz/matejseykora/shop/ShopApplicationTests.java
+++ src/test/java/cz/matejseykora/shop/ShopApplicationTests.java
@@ -1,13 +1,19 @@
 package cz.matejseykora.shop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class ShopApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void test() {
+		int a = 2;
+		int b = 3;
+		Assertions.assertEquals(5,a+b);
 	}
-
 }
