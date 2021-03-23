c614dd837cb2af523dc504609a491cdf5d7d9608
matejseykora <matej.seykora@gmail.com>
2021-03-23 15:48:09 +0100

    added delete and update methods

--- src/main/java/cz/matejseykora/shop/db/service/api/ProductService.java
+++ src/main/java/cz/matejseykora/shop/db/service/api/ProductService.java
@@ -1,10 +1,12 @@
 package cz.matejseykora.shop.db.service.api;
 
+import cz.matejseykora.shop.db.service.api.request.UpdateProductRequest;
 import cz.matejseykora.shop.domain.Product;
 import org.springframework.lang.Nullable;
 
 import java.util.List;
 
+// CRUD operations listed here
 public interface ProductService {
     List<Product> getProducts();
 
@@ -13,4 +15,9 @@ public interface ProductService {
 
     @Nullable
     Integer add(Product product); // returns generated id
+
+    @Nullable
+    void delete(int id);
+
+    void update(int id, UpdateProductRequest request);
 }
