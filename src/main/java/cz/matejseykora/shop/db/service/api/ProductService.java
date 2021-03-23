19b8b42bf452669a47a65230cd7e04bbc9b1b29a
matejseykora <matej.seykora@gmail.com>
2021-03-23 13:39:34 +0100

    ProductService interface creation

--- src/main/java/cz/matejseykora/shop/db/service/api/ProductService.java
+++ src/main/java/cz/matejseykora/shop/db/service/api/ProductService.java
@@ -1,5 +1,16 @@
 package cz.matejseykora.shop.db.service.api;
 
-public interface ProductService {
+import cz.matejseykora.shop.domain.Product;
+import org.springframework.lang.Nullable;
 
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getProducts();
+
+    @Nullable
+    Product get(int id);
+
+    @Nullable
+    Integer add(Product product); // returns generated id
 }
